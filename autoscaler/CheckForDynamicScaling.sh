a263215dfdf229aa2f11730807a641d174aaf876
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-17 02:32:36 +0000

    Update CheckForDynamicScaling.sh

--- autoscaler/CheckForDynamicScaling.sh
+++ autoscaler/CheckForDynamicScaling.sh
@@ -3,9 +3,15 @@
 
 instanceid="`/usr/bin/aws ec2 describe-instances --filter 'Name=tag:ScalingStyle,Values=Dynamic' 'Name=instance-state-name,Values=running' | /usr/bin/jq '.Reservations[].Instances[].InstanceId'`"
 
-if ( [ "${instanceid}" = "" ] )
+if ( [ ! -d ${HOME}/config/dynamicscalingprocessing ] )
+then
+    /bin/mkdir -p ${HOME}/config/dynamicscalingprocessing
+fi
+
+if ( [ "${instanceid}" = "" ] || [ -f ${HOME}/config/dynamicscalingprocessing/${instanceid} ] )
 then
     /bin/echo "0"
 else
+    /bin/touch ${HOME}/config/dynamicscalingprocessing/${instanceid}
     /bin/echo "1"
 fi
