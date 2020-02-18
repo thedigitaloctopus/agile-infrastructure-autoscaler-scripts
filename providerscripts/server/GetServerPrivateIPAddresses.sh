fbcff185d3712b22d197611ecbc3c99edff9ca89
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-18 21:32:19 +0000

    Update GetServerPrivateIPAddresses.sh

--- providerscripts/server/GetServerPrivateIPAddresses.sh
+++ providerscripts/server/GetServerPrivateIPAddresses.sh
@@ -50,7 +50,7 @@ fi
 
 if ( [ -f ${HOME}/LINODE ] || [ "${cloudhost}" = "linode" ] )
 then
-    /usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /usr/bin/awk '{print $NF}'
+   /usr/local/bin/linode-cli linodes list --text | /bin/grep ${server_type} | /bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /bin/grep "192.168"
 fi
 
 if ( [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
