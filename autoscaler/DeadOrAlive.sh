79e9940e43d04b0a7db00aedd93b3bb039d220c5
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-02 09:51:52 +0100

    Update DeadOrAlive.sh

--- autoscaler/DeadOrAlive.sh
+++ autoscaler/DeadOrAlive.sh
@@ -109,12 +109,13 @@ fi
 #get a list of all ip addresses which we consider should be active
 allliveips="`${HOME}/autoscaler/GetDNSIPs.sh`"
 
-CLOUDHOST="`/bin/ls ${HOME}/.ssh/CLOUDHOST:* | /usr/bin/awk -F':' '{print $NF}'`"
-BUILD_IDENTIFIER="`/bin/ls ${HOME}/.ssh/BUILDIDENTIFIER:* | /usr/bin/awk -F':' '{print $NF}'`"
-ALGORITHM="`/bin/ls ${HOME}/.ssh/ALGORITHM:* | /usr/bin/awk -F':' '{print $NF}'`"
-SERVER_USER="`/bin/ls ${HOME}/.ssh/SERVERUSER:* | /usr/bin/awk -F':' '{print $NF}'`"
-SERVER_USER_PASSWORD="`/bin/ls ${HOME}/.ssh/SERVERUSERPASSWORD:* | /usr/bin/awk -F':' '{print $NF}'`"
-SSH_PORT="`/bin/ls ${HOME}/.ssh/SSH_PORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+CLOUDHOST="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'CLOUDHOST'`"
+BUILD_IDENTIFIER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
+ALGORITHM="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'ALGORITHM'`"
+SERVER_USER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'SERVERUSER'`"
+SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
+SSH_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'SSH_PORT'`"
+
 SUDO=" DEBIAN_FRONTEND=noninteractive /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
 
 for ip in ${allliveips}
@@ -150,15 +151,16 @@ do
         /bin/echo "${0} `/bin/date` : ${ip} is being destroyed because it was unresponsive" >> ${HOME}/logs/MonitoringLog.log
         ${HOME}/providerscripts/server/DestroyServer.sh ${ip} ${CLOUDHOST}
         
-        DBaaS_DBSECURITYGROUP="`/bin/ls ${HOME}/.ssh/DBaaSDBSECURITYGROUP:* | /usr/bin/awk -F':' '{print $NF}'`"
+	DBaaS_DBSECURITYGROUP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBaaSDBSECURITYGROUP'`"
+
         if ( [ "${DBaaS_DBSECURITYGROUP}" != "" ] )
         then
             IP_TO_DENY="${ip}"
             . ${HOME}/providerscripts/server/DenyDBAccess.sh
         fi
         
-        INMEMORYCACHING_SECURITY_GROUP="`/bin/ls ${HOME}/.ssh/INMEMORYCACHINGSECURITYGROUP:* | /usr/bin/awk -F':' '{print $NF}'`"
-        INMEMORYCACHING_PORT="`/bin/ls ${HOME}/.ssh/INMEMORYCACHINGPORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+        INMEMORYCACHING_SECURITY_GROUP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'INMEMORYCACHINGSECURITYGROUP'`"
+	INMEMORYCACHING_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'INMEMORYCACHINGPORT'`"
 
         if ( [ "${INMEMORYCACHING_SECURITY_GROUP}" != "" ] )
         then
@@ -232,15 +234,17 @@ then
             /bin/echo "${0} `/bin/date` : ${ip} is being destroyed because it was considered orphaned from the DNS system" >> ${HOME}/logs/MonitoringLog.log
 
             ${HOME}/providerscripts/server/DestroyServer.sh ${ip} ${CLOUDHOST}
-            DBaaS_DBSECURITYGROUP="`/bin/ls ${HOME}/.ssh/DBaaSDBSECURITYGROUP:* | /usr/bin/awk -F':' '{print $NF}'`"
+	    
+            DBaaS_DBSECURITYGROUP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBaaSDBSECURITYGROUP'`"
+
             if ( [ "${DBaaS_DBSECURITYGROUP}" != "" ] ) 
             then
                 IP_TO_DENY="${ip}"
                 . ${HOME}/providerscripts/server/DenyDBAccess.sh
             fi
-            
-            INMEMORYCACHING_SECURITY_GROUP="`/bin/ls ${HOME}/.ssh/INMEMORYCACHINGSECURITYGROUP:* | /usr/bin/awk -F':' '{print $NF}'`"
-            INMEMORYCACHING_PORT="`/bin/ls ${HOME}/.ssh/INMEMORYCACHINGPORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+	    
+	    INMEMORYCACHING_SECURITY_GROUP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'INMEMORYCACHINGSECURITYGROUP'`"
+            INMEMORYCACHING_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'INMEMORYCACHINGPORT'`"
 
             if ( [ "${INMEMORYCACHING_SECURITY_GROUP}" != "" ] )
             then
