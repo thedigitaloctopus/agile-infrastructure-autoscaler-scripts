7d3c7b476d05f45cec74fa71a43ac3678ec90032
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-02 13:18:26 +0100

    Update ListAvailableRegions.sh

--- providerscripts/cloudhost/ListAvailableRegions.sh
+++ providerscripts/cloudhost/ListAvailableRegions.sh
@@ -36,7 +36,7 @@ then
 fi
 if (  [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`/bin/ls ${HOME}/.ssh/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
+    export VULTR_API_KEY="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'VULTRAPIKEY'`"
     /bin/sleep 1
     /usr/bin/vultr regions | /usr/bin/awk '{print $NF}' | /bin/sed 's/CODE//g'
 fi
