0a4666097da91d33b4c10d0d4c42379698cdccff
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-06 20:31:53 +0100

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -55,6 +55,16 @@ then
     /bin/sleep 5
 fi
 
+NEW_BUILD_CLIENT_IP="`/bin/ls /tmp/BUILDCLIENTIP/* | awk -F'/' '{print $NF}'`"
+if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${NEW_BUILD_CLIENT_IP} | /bin/grep ALLOW`" = "" ] )
+then
+    /usr/sbin/ufw default deny incoming
+    /usr/sbin/ufw default allow outgoing
+    /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from ${NEW_BUILD_CLIENT_IP} to any port ${SSH_PORT}
+    /bin/sleep 5
+fi
+
+
 for autoscalerip in `/bin/ls ${HOME}/config/autoscalerip`
 do
     if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${autoscalerip} | /bin/grep ALLOW`" = "" ] )
