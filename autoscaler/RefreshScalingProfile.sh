ce4ebe1dd647269dd2c5f7a8dfa5ae917b522e49
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-10 14:16:46 +0100

    Update RefreshScalingProfile.sh

--- autoscaler/RefreshScalingProfile.sh
+++ autoscaler/RefreshScalingProfile.sh
@@ -1,8 +1,11 @@
 
 #With s3fs if the user updates the profile.cnf file without changing the number of bytes s3fs assumes it didn't change
 #So, if we find it has been updated on one of the machines, add an extra space to it in order to signal a change
-if ( [ "`/usr/bin/find ${HOME}/config/scalingprofile/profile.cnf -mmin -2`" != "" ] )
+if ( [ -f ${HOME}/config/scalingprofile/profile.cnf ] )
 then
-    /bin/echo " " >> ${HOME}/config/scalingprofile/profile.cnf
-    /bin/sleep 140
-fi    
+    if ( [ "`/usr/bin/find ${HOME}/config/scalingprofile/profile.cnf -mmin -2`" != "" ] )
+    then
+       /bin/echo " " >> ${HOME}/config/scalingprofile/profile.cnf
+       /bin/sleep 140
+    fi
+fi
