b9ba415407f80c24fef648cd50bace5fc278670d
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-18 19:21:59 +0100

    Update DeleteSSHKeyPair.sh

--- providerscripts/security/DeleteSSHKeyPair.sh
+++ providerscripts/security/DeleteSSHKeyPair.sh
@@ -48,7 +48,7 @@ then
 fi
 if ( [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'VULTRAPIKEY'`"
+    export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
 
     /bin/sleep 1
     for key_id in `/usr/bin/vultr sshkeys | /usr/bin/awk '{print $1}'`
