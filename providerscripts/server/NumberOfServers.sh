86f4bcde44b4aef5c7112e24c82db0b766cc49d0
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-18 19:27:31 +0100

    Update NumberOfServers.sh

--- providerscripts/server/NumberOfServers.sh
+++ providerscripts/server/NumberOfServers.sh
@@ -55,7 +55,7 @@ fi
 
 if ( [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'VULTRAPIKEY'`"
+    export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
     /bin/sleep 1
     server_type="`/bin/echo ${server_type} | /usr/bin/cut -c -25`"
     /usr/bin/vultr server list | /bin/grep ${server_type} | /usr/bin/awk '{print $3}' | /bin/sed 's/IP//g' | /bin/sed '/^$/d' | /usr/bin/wc -l
