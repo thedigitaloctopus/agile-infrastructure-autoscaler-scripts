73ff41c3a03342e66f65ee7792528b9afc4821b8
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-18 19:23:42 +0100

    Update DestroyServer.sh

--- providerscripts/server/DestroyServer.sh
+++ providerscripts/server/DestroyServer.sh
@@ -110,7 +110,7 @@ fi
 
 if ( [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'VULTRAPIKEY'`"
+    export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
 
     if ( [ "${server_ip}" != "" ] )
     then
