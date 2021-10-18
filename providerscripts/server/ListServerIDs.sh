f145b8c1775ee162f472555a631dfa9473b481ee
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-18 19:27:12 +0100

    Update ListServerIDs.sh

--- providerscripts/server/ListServerIDs.sh
+++ providerscripts/server/ListServerIDs.sh
@@ -54,7 +54,7 @@ fi
 
 if ( [ -f ${HOME}/VULTR ] || [ "${cloudhost}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'VULTRAPIKEY'`"
+    export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
     /bin/sleep 1
     /usr/bin/vultr server list | /usr/bin/awk '{print $1}' | /bin/sed 's/SUBID//g' | /bin/sed '/^$/d'
 fi
