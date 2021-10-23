e5849f89bba0c5245a6e2a4efc40d218832a247e
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-23 22:03:21 +0100

    Update GetRecordID.sh

--- providerscripts/dns/GetRecordID.sh
+++ providerscripts/dns/GetRecordID.sh
@@ -87,5 +87,9 @@ if ( [ "${dns}" = "vultr" ] )
 then
     HOME="`/bin/cat /home/homedir.dat`"
     export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
+    #Clonk
     /usr/bin/vultr dns record list -d ${domainurl} | /bin/grep  ${ip} | /usr/bin/awk '{print $1}'
+    #Official
+    /usr/bin/vultr dns record list ${domainurl} | /bin/grep ${ip} | /usr/bin/awk '{print $1}'
+
 fi
