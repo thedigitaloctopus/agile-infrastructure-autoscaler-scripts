7c2045f55f99914f8b56647dae67fd1082acde78
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-23 22:01:42 +0100

    Update GetIPAddresses.sh

--- providerscripts/dns/GetIPAddresses.sh
+++ providerscripts/dns/GetIPAddresses.sh
@@ -76,5 +76,8 @@ if ( [ "${dns}" = "vultr" ] )
 then
     HOME="`/bin/cat /home/homedir.dat`"
     export VULTR_API_KEY="`/bin/ls ${HOME}/.config/VULTRAPIKEY:* | /usr/bin/awk -F':' '{print $NF}'`"
+    #Clonk
     /usr/bin/vultr dns record list -d ${domain_url} | /bin/grep  ${subdomain} | /usr/bin/awk '{print $4}'
+    #Official
+    /usr/bin/vultr dns record list ${domain_url} | /bin/grep ${subdomain} | /usr/bin/awk '{print $4}'
 fi
