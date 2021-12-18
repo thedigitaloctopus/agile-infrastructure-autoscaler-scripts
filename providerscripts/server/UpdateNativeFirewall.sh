83f24b7644327a5d375000e43270952019ce89f5
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-12-18 22:52:37 +0000

    Update UpdateNativeFirewall.sh

--- providerscripts/server/UpdateNativeFirewall.sh
+++ providerscripts/server/UpdateNativeFirewall.sh
@@ -30,21 +30,44 @@ fi
 
 if ( [ -f ${HOME}/EXOSCALE ] )
 then
-    if ( [ "${2}" = "" ] )
-    then
-        /usr/bin/exo compute security-group rule add adt --network ${1}/32 --port 1-65535
-    else
-        /usr/bin/exo compute security-group rule add adt --network ${1}/32 --port ${2}
-    fi
+    
+    allips="`/bin/cat ${HOME}/runtime/ipsforfirewall`"
 
-    #Delete the general access rule, if it exists that we setup during the build process
+   # if ( [ "${2}" = "" ] )
+   # then
+   #     /usr/bin/exo compute security-group rule add adt --network ${1}/32 --port 1-65535
+   # else
+   #    /usr/bin/exo compute security-group rule add adt --network ${1}/32 --port ${2}
+   #fi
+    
+   for ip in ${allips}
+   do
+       /usr/bin/exo compute security-group rule add adt --network ${ip} --port ${SSH_PORT}
+       /usr/bin/exo compute security-group rule add adt --network ${ip} --port ${DB_PORT}
+   done
+   id=""
+   id="`/usr/bin/exo -O json compute security-group show adt | jq --argjson tmp_port "443" '(.ingress_rules[] | select (.start_port == $tmp_port) | select (.network == "0.0.0.0/0") | .id)' | /bin/sed 's/"//g'`"
+   if ( [ "${id}" = "" ] )
+   then
+       /usr/bin/exo compute security-group rule add adt --network 0.0.0.0/0 --port 443
+   fi
+   
+   id=""
+   id="`/usr/bin/exo -O json compute security-group show adt | jq --argjson tmp_port "80" '(.ingress_rules[] | select (.start_port == $tmp_port) | select (.network == "0.0.0.0/0") | .id)' | /bin/sed 's/"//g'`"
+   if ( [ "${id}" = "" ] )
+   then
+       /usr/bin/exo compute security-group rule add adt --network 0.0.0.0/0 --port 80
+   fi
 
-    if ( [ "${2}" != "" ] && [ "${2}" != "443" ] && [ "${2}" != "80" ] )
-    then
-        port="${2}"
-        id="`/usr/bin/exo -O json compute security-group show adt | jq --argjson tmp_port "$port" '(.ingress_rules[] | select (.start_port == $tmp_port) | select (.network == "0.0.0.0/0") | .id)' | /bin/sed 's/"//g'`"
-        /usr/bin/exo  compute security-group rule delete -f adt ${id}
-    fi
+
+    #Delete the general access rule, if it exists, that we setup during the build process
+
+   # if ( [ "${2}" != "" ] && [ "${2}" != "443" ] && [ "${2}" != "80" ] )
+   # then
+   #     port="${2}"
+   #     id="`/usr/bin/exo -O json compute security-group show adt | jq --argjson tmp_port "$port" '(.ingress_rules[] | select (.start_port == $tmp_port) | select (.network == "0.0.0.0/0") | .id)' | /bin/sed 's/"//g'`"
+   #     /usr/bin/exo  compute security-group rule delete -f adt ${id}
+   # fi
 fi
 
 if ( [ -f ${HOME}/LINODE ] )
@@ -52,13 +75,13 @@ then
 
     allips="`/bin/cat ${HOME}/runtime/ipsforfirewall`"
     
-    linode_id=""
+  #  linode_id=""
     
-    if ( [ "${1}" != "" ] )
-    then
-        allips="${allips} ${1}"
+  #  if ( [ "${1}" != "" ] )
+   # then
+       # allips="${allips} ${1}"
         linode_id="`/usr/local/bin/linode-cli --json linodes list | jq --arg tmp_ip "${1}" '.[] | select (.ipv4 | tostring | contains ($tmp_ip))'.id`"
-    fi
+  #  fi
 
     firewall_id="`/usr/local/bin/linode-cli --json firewalls list | jq '.[] | select (.label == "adt" ).id'`"
     
