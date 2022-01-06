1a2dd57e0717b4472b6c82f1332b9e5ad894bb1c
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-06 19:29:22 +0000

    Update GetProxyDNSIPs.sh

--- providerscripts/security/firewall/GetProxyDNSIPs.sh
+++ providerscripts/security/firewall/GetProxyDNSIPs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 alldnsproxyips=""
 CLOUDHOST="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'CLOUDHOST'`"
 DNS_CHOICE="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DNSCHOICE'`"
@@ -26,11 +28,13 @@ then
         alldnsproxyips="103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 104.16.0.0/13 104.24.0.0/14 108.162.192.0/18 141.101.64.0/18 162.158.0.0/15 172.64.0.0/13 173.245.48.0/20 188.114.96.0/20 190.93.240.0/20 197.234.240.0/22 198.41.128.0/17 131.0.72.0/22 199.27.128.0/21"
         autoscalerips="`/bin/echo ${autoscalerips} | /bin/sed 's/,/ /g'`"
         alldnsproxyips="${autoscalerips} ${alldnsproxyips}"
+        alldnsproxyips="`/bin/echo ${alldnsproxyips} | /bin/sed 's/"//g'`"
     fi
     if ( [ "${CLOUDHOST}" = "exoscale" ] )
     then
         alldnsproxyips="103.21.244.0/22,103.22.200.0/22,103.31.4.0/22,104.16.0.0/13,104.24.0.0/14,108.162.192.0/18,141.101.64.0/18,162.158.0.0/15,172.64.0.0/13,173.245.48.0/20,188.114.96.0/20,190.93.240.0/20,197.234.240.0/22,198.41.128.0/17,131.0.72.0/22,199.27.128.0/21"
         alldnsproxyips="${autoscalerips},${alldnsproxyips}"
+        alldnsproxyips="`/bin/echo ${alldnsproxyips} | /bin/sed 's/"//g'`"
     fi
     if ( [ "${CLOUDHOST}" = "linode" ] )
     then
