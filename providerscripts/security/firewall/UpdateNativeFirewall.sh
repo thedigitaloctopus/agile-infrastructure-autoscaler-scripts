0eaee2f116a10aa1c94dd10291ee54fbb99599c5
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-06 16:49:58 +0000

    Update UpdateNativeFirewall.sh

--- providerscripts/security/firewall/UpdateNativeFirewall.sh
+++ providerscripts/security/firewall/UpdateNativeFirewall.sh
@@ -95,7 +95,7 @@ then
         websever_firewall_id="`/usr/local/bin/doctl -o json compute firewall list | jq '.[] | select (.name == "adt-webserver-machines" ).id' | /bin/sed 's/"//g'`"
     fi
     
-    droplet_ids="`/usr/local/bin/doctl compute droplet list | /bin/grep 'webserver' | /usr/bin/awk '{print $1}' | /usr/bin/tail -n +2`"    
+    droplet_ids="`/usr/local/bin/doctl compute droplet list | /bin/grep 'webserver' | /usr/bin/awk '{print $1}' | /bin/sed 's/ //g'`"    
 
     /usr/local/bin/doctl compute firewall add-rules ${webserver_firewall_id} --inbound-rules "${standard_rules}"
     /usr/local/bin/doctl compute firewall add-droplets ${webserver_firewall_id} --droplet-ids ${droplet_ids}
