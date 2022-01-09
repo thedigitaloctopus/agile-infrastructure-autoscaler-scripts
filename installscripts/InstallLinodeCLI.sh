9ac2ba71aea09b3127d0969c82a481dda89ef50f
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-09 19:03:11 +0000

    Update InstallLinodeCLI.sh

--- installscripts/InstallLinodeCLI.sh
+++ installscripts/InstallLinodeCLI.sh
@@ -27,13 +27,13 @@ fi
 
 if ( [ "${BUILD_OS}" = "ubuntu" ] )
 then
-    /usr/bin/apt-get -qq -y install python3-setuptools
+    /usr/bin/apt-get  -o DPkg::Lock::Timeout=-1 -qq -y install python3-setuptools
     /usr/bin/pip3 install linode-cli --upgrade
 fi
 
 if ( [ "${BUILD_OS}" = "debian" ] )
 then
-    /usr/bin/apt-get -qq -y install python3-setuptools
+    /usr/bin/apt-get  -o DPkg::Lock::Timeout=-1 -qq -y install python3-setuptools
     /usr/bin/pip3 install linode-cli --upgrade
 fi
 
