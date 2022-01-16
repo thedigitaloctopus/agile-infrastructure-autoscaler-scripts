81daefd180c72e0b6720db9fb94b47104ba1b14e
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-16 18:58:27 +0000

    Update InitialiseCron.sh

--- providerscripts/utilities/InitialiseCron.sh
+++ providerscripts/utilities/InitialiseCron.sh
@@ -37,6 +37,8 @@
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/cron/SetupFirewallFromCron.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/autoscaler/PurgeDetachedIPs.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/autoscaler/RefreshScalingProfile.sh" >> /var/spool/cron/crontabs/root
+/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && /bin/sleep 20 && ${HOME}/providerscripts/security/firewall/MonitorForLaptopIPs.sh" >> /var/spool/cron/crontabs/root
+
 
 #These scripts are set to run every 5 minutes
 /bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/security/MonitorFirewall.sh" >> /var/spool/cron/crontabs/root
