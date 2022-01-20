af625a71b75e858bfdf6a28d1440d6a82d8b6a7a
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-20 14:43:19 +0000

    Update GetIP.sh

--- providerscripts/utilities/GetIP.sh
+++ providerscripts/utilities/GetIP.sh
@@ -41,17 +41,6 @@ if ( [ -f ${HOME}/VULTR ] && [ ! -f ${HOME}/runtime/NETCONFIGURED ] )
 then
     ip="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYIP'`"
     
-    uuid="`/bin/cat /boot/grub/grub.cfg | /bin/grep vmlinuz | /usr/bin/head -1 | /bin/sed 's/.*UUID=//g' | /usr/bin/awk '{print $1}'`"
-    
-    if ( [ ! -L /dev/disk/byuuid/${uuid} ] )
-    then
-       if ( [ ! -d /dev/disk/byuuid/ ] )
-       then
-           /bin/mkdir -p /dev/disk/byuuid/
-       fi
-       /usr/bin/ln -s /dev/vda1 /dev/disk/byuuid/${uuid} 
-    fi
-    
     if ( [ "${BUILDOS}" = "debian" ] )
     then
         if ( [ "${BUILDOSVERSION}" = "10" ] )
