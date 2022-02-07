762961fd3f9f474672b4b63d0d665bfc21ea93bf
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-07 13:36:39 +0000

    Update GetServerPrivateIPAddresses.sh

--- providerscripts/server/GetServerPrivateIPAddresses.sh
+++ providerscripts/server/GetServerPrivateIPAddresses.sh
@@ -100,6 +100,6 @@ fi
 
 if ( [ -f ${HOME}/AWS ] || [ "${cloudhost}" = "aws" ] )
 then 
-    /usr/bin/aws ec2 describe-instances --filter "Name=tag:descriptiveName,Values=${server_type}*" "Name=instance-state-name,Values=running" | /usr/bin/jq '.Reservations[].Instances[].PrivateIpAddress' | /bin/sed 's/\"//g'
+    /usr/bin/aws ec2 describe-instances --filter "Name=tag:descriptiveName,Values=*${server_type}*" "Name=instance-state-name,Values=running" | /usr/bin/jq '.Reservations[].Instances[].PrivateIpAddress' | /bin/sed 's/\"//g'
 fi
 
