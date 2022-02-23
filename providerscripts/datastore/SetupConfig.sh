3c390d176c83caf9b9be238acd6abbdf0da0264c
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-23 22:18:04 +0000

    Update SetupConfig.sh

--- providerscripts/datastore/SetupConfig.sh
+++ providerscripts/datastore/SetupConfig.sh
@@ -92,7 +92,7 @@ then
             done
         else
             /usr/bin/s3cmd mb s3://${configbucket}
-            /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
+            /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2,max_stat_cache_size=10000,stat_cache_expire=30,multireq_max=50 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
         fi
     fi
 fi
@@ -102,7 +102,7 @@ then
     export AWSACCESSKEYID=`/bin/grep 'access_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     export AWSSECRETACCESSKEY=`/bin/grep 'secret_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     /usr/bin/s3cmd mb s3://${configbucket}
-    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
+    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2,max_stat_cache_size=10000,stat_cache_expire=30,multireq_max=50 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
 fi
 
 if ( [ "${DATASTORE_PROVIDER}" = "exoscale" ] )
@@ -110,7 +110,7 @@ then
     export AWSACCESSKEYID=`/bin/grep 'access_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     export AWSSECRETACCESSKEY=`/bin/grep 'secret_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     /usr/bin/s3cmd mb s3://${configbucket}
-    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style -ourl=https://${endpoint} ${configbucket} ${HOME}/config
+    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2,max_stat_cache_size=10000,stat_cache_expire=30,multireq_max=50 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
 fi
 
 if ( [ "${DATASTORE_PROVIDER}" = "linode" ] )
@@ -118,7 +118,7 @@ then
     export AWSACCESSKEYID=`/bin/grep 'access_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     export AWSSECRETACCESSKEY=`/bin/grep 'secret_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     /usr/bin/s3cmd mb s3://${configbucket}
-    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style -ourl=https://${endpoint} ${configbucket} ${HOME}/config
+    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2,max_stat_cache_size=10000,stat_cache_expire=30,multireq_max=50 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
 fi
 
 if ( [ "${DATASTORE_PROVIDER}" = "vultr" ] )
@@ -126,7 +126,7 @@ then
     export AWSACCESSKEYID=`/bin/grep 'access_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     export AWSSECRETACCESSKEY=`/bin/grep 'secret_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
     /usr/bin/s3cmd mb s3://${configbucket}
-    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
+    /usr/bin/s3fs -o nonempty,allow_other,use_path_request_style,sigv2,max_stat_cache_size=10000,stat_cache_expire=30,multireq_max=50 -ourl=https://${endpoint} ${configbucket} ${HOME}/config
 fi
 
 #SERVER_USER="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'SERVERUSER'`"
