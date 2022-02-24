e546bf3e76eddd04f18d43cc5376c3fb9d8e7633
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 23:59:34 +0000

    Update InitialiseCron.sh

--- providerscripts/utilities/InitialiseCron.sh
+++ providerscripts/utilities/InitialiseCron.sh
@@ -41,9 +41,6 @@
 #These scripts are set to run every 5 minutes
 /bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/security/MonitorFirewall.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/autoscaler/MonitorForSlowBuilds.sh" >> /var/spool/cron/crontabs/root
-/bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/AuditForLowCPUStates.sh 10" >> /var/spool/cron/crontabs/root
-/bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/AuditForLowMemoryStates.sh 90" >> /var/spool/cron/crontabs/root
-/bin/echo "*/5 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/AuditForLowDiskStates.sh 100000" >> /var/spool/cron/crontabs/root
 
 #This script will run every 10 minutes
 /bin/echo "*/10 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/EnforcePermissions.sh" >> /var/spool/cron/crontabs/root
