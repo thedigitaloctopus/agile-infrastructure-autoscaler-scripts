a84210ecc3d1c9ba466dcf0196ec2f262fc726ee
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-24 20:54:09 +0000

    Update MonitorForSlowBuilds.sh

--- autoscaler/MonitorForSlowBuilds.sh
+++ autoscaler/MonitorForSlowBuilds.sh
@@ -22,7 +22,7 @@
 #######################################################################################################
 #set -x
 
-if ( [ ! -f ${HOME}/config/INSTALLEDSUCCESSFULLY ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "INSTALLEDSUCCESSFULLY"`" = "0" ] )
 then
     exit
 fi
